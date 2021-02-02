a5941694fdc9839b567e1e503e6cbd224cfc4c61
vaz_mf <vazfernandam@gmail.com>
2021-02-02 08:49:15 -0300

    Ajustando funções

--- src/lib/download/__init__.py
+++ src/lib/download/__init__.py
@@ -1,34 +1,45 @@
-from pip._vendor.requests.utils import from_key_val_list
 from pytube import YouTube, Playlist
 from lib.converter import *
 
+music_folder = "/home/vaz_mf/Música"
+video_folder = "/home/vaz_mf/Vídeos"
+
 
 def download_one_music(yt_link):
-    folder = "/home/vaz_mf/Música"
-    yt = YouTube(yt_link).streams.filter(only_audio=True).first().download(folder)
+    yt = YouTube(yt_link).streams.filter(only_audio=True).first().download(music_folder)
     converter(yt)
-    print(f'\033[32m{yt.title()} was successfully downloaded.\033m')
+    print(f'{yt.title()} was successfully downloaded.')
 
 
 def download_playlist(yt_link):
-    folder = "/home/vaz_mf/Música"
     playlist = Playlist(yt_link)
-    for url in playlist:
-        yt = YouTube(url).streams.filter(only_audio=True).first().download(folder)
-        converter(yt)
-    print(f'\033[32mThe playlist was successfully downloaded.\033[m')
+    try:
+        for url in playlist:
+            yt = YouTube(url).streams.filter(only_audio=True).first().download(music_folder)
+            converter(yt)
+    except:
+        print("[ERROR] Please, try again")
+    else:
+        print(f'The playlist was successfully downloaded.')
+
 
 
 def download_one_video(yt_link):
-    folder = "/home/vaz_mf/Vídeos"
-    yt = YouTube(yt_link).streams.get_highest_resolution().download(folder)
-    print(f'\033[32m{yt.title()} was successfully downloaded.\033[m')
+    try:
+        yt = YouTube(yt_link).streams.get_highest_resolution().download(video_folder)
+    except:
+        print("[ERROR] Please, try again")
+    else:
+        print(f'{yt.title()} was successfully downloaded.')
 
 
 def download_video_playlist(yt_link):
-    folder = "/home/vaz_mf/Vídeos"
     playlist = Playlist(yt_link)
-    for url in playlist:
-        yt = YouTube(url).streams.filter().get_highest_resolution().download(folder)
-    print(f'\033[32mThe playlist was successfully downloaded.\033[m')
+    try:
+        for url in playlist:
+            yt = YouTube(url).streams.filter().get_highest_resolution().download(video_folder)
+    except:
+        print("[ERROR] Please, try again")
+    else:
+        print(f'The playlist was successfully downloaded.')
 
